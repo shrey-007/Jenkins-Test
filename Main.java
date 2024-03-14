12e045d1ef1464720eaefdfb8966ba168cf855fe
shreySaxena <shreyyerhs07@gmail.com>
2024-03-14 22:06:25 +0530

    some changes

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
   
    public static void main(String args[]){
        System.out.println("Hi this is java program running through jenkins");
+       System.out.println("Some changes in code");
    } 
 }
